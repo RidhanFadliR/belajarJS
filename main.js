5439afdbf2834be0d2b1cdcd92078d38d2eaa44b
RidhanFadliR <ridhanfadli@upi.edu>
2023-10-12 14:51:36 +0700

    add main.js

---
+++ main.js
@@ -0,0 +1,88 @@
+// console.log("Hello World")
+// alert("Notifikasi")
+// prompt("Mahasiswa UNNES")
+
+// var Promnet = "I Love Promnet"
+// console.log(Promnet)
+// var Promnet = "I Hate Promnet"
+// console.log(Promnet)
+
+// let Promnet = "I Love Promnet"
+// console.log(Promnet)
+// Promnet = "I Hate Promnet"
+// console.log(Promnet)
+
+// const Promnet = "I Love Promnet"
+// console.log(Promnet)
+// Promnet = "I Hate Promnet"
+// console.log(Promnet)
+
+// let totalpoin = prompt("Masukan Point Anda")
+//     if(totalpoin > 100){
+//         document.write("<h1>Congratulation<h1>");
+//     }
+//     else{
+//         document.write("<h1>Thank You<h1>");
+//     }
+
+// let x = 6;
+// let y = 3;
+
+// if (x < 10 && y > 1) {
+//   console.log("a: true");
+//   document.write("<h1>BENAR<h1>");
+// } else {
+//   console.log("a: false");
+//   document.write("<h1>SALAH<h1>");
+// }
+
+// if (x < 10 && y < 1) {
+//   console.log("b: true");
+//   document.write("<h1>BENAR<h1>");
+// } else {
+//   console.log("b: false");
+//   document.write("<h1>SALAH<h1>");
+// }
+
+// if (x == 5 || y == 5) {
+//   console.log("c: true");
+//   document.write("<h1>BENAR<h1>");
+// } else {
+//   console.log("c: false");
+//   document.write("<h1>SALAH<h1>");
+// }
+
+// if (x == 6 || y == 5) {
+//   console.log("d: true");
+//   document.write("<h1>BENAR<h1>");
+// } else {
+//   console.log("d: false");
+//   document.write("<h1>SALAH<h1>");
+// }
+
+// if (!(x == y)) {
+//   console.log("e: true");
+//   document.write("<h1>BENAR<h1>");
+// } else {
+//   console.log("e: false");
+//   document.write("<h1>SALAH<h1>");
+// }
+
+// let x = 6;
+// let y = 3;
+// console.log(x < 10 && y > 1)
+// console.log(x < 10 && y < 1)
+// console.log(x == 5 || y == 5)
+// console.log(x == 6 || y == 5)
+// console.log(!(x == y))
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+
+// })
