6b51005be9dc4b0e3defe48daa3b7a686752bf6a
RidhanFadliR <ridhanfadli@upi.edu>
2023-10-16 12:15:23 +0700

    update index.html and main.js

--- main.js
+++ main.js
@@ -84,5 +84,61 @@
 //     let isi = input.value
 //     console.log(isi)
 //     p.innerHTML = isi
-
 // })
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
+
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+todoForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const todoText = todoInput.value.trim();
+
+    if (todoText !== "") {
+        const todoItem = document.createElement("div");
+        todoItem.classList.add("todo-item");
+
+        const todoTextSpan = document.createElement("span");
+        todoTextSpan.textContent = todoText;
+
+        const completeCheckbox = document.createElement("input");
+        completeCheckbox.type = "checkbox";
+        completeCheckbox.classList.add("complete-checkbox");
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "X";
+        deleteButton.classList.add("X-button");
+
+        todoItem.appendChild(completeCheckbox);
+        todoItem.appendChild(todoTextSpan);
+        todoItem.appendChild(deleteButton);
+
+        todoList.appendChild(todoItem);
+        todoInput.value = "";
+
+        completeCheckbox.addEventListener("change", function() {
+            if (completeCheckbox.checked) {
+                const deletedText = document.createElement("del");
+                deletedText.textContent = todoText;
+                todoTextSpan.textContent = ''; 
+                todoTextSpan.appendChild(deletedText); 
+            } else {
+                todoTextSpan.textContent = todoText; 
+            }
+        });
+        
+        deleteButton.addEventListener("click", function() {
+            todoItem.remove();
+        });
+    }
+});
